18f91daa4496ae9b86b57f21de252ba11f40a0c3
Jhastyn <jpayehuancar@unsa.edu.pe>
2023-07-14 10:33:11 -0500

    Creando modelos 'Language' 'Framework'

--- example/models.py
+++ example/models.py
@@ -13,3 +13,16 @@ class DateExample(models.Model):
 
 class NullExample(models.Model):
 	col = models.CharField(max_length=10, blank=True, null=True)
+
+class Language(models.Model):
+    name = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
