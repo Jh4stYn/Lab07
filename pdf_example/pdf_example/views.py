2d4284e5f822dd9e1827dea8e3e7e76d45eaf7ac
Jhastyn <jpayehuancar@unsa.edu.pe>
2023-07-14 22:11:13 -0500

    Modificamos el método get(), añadiendo la función 'download to pdf' en 'views.py'

--- pdf_example/pdf_example/views.py
+++ pdf_example/pdf_example/views.py
@@ -18,7 +18,11 @@ class GeneratePdf(View):
         if pdf:
             response = HttpResponse(pdf, content_type='application/pdf')
             filename = "Invoice_%s.pdf" %("12341231")
-            content = "inline; filename='%s'" %(filename)
+            content = "inline; filename=%s" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" %(filename)
+            print(content)
             response['Content-Disposition'] = content
             return response
         return HttpResponse("Not found")
